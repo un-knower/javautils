ee62cc12e3dfb7ba93ddc0c5496ef8c4a5601fee
jiadongpo <jia187516>
2018-04-23 14:00:20 +0800

    性能测试

---
+++ src/test/java/com/cenrise/performancetest/HoldCPUMain.java
@@ -0,0 +1,32 @@
+
+
+public class HoldCPUMain {
+	public static class HoldCPUTask implements Runnable {
+		@Override
+		public void run() {
+			while (true) {
+				double a = Math.random() * Math.random();
+			}
+		}
+	}
+
+	public static class LazyTask implements Runnable {
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					Thread.sleep(1000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new Thread(new HoldCPUTask()).start();
+		new Thread(new LazyTask()).start();
+		new Thread(new LazyTask()).start();
+		new Thread(new LazyTask()).start();
+	}
+}
